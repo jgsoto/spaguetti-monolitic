894fb98706a59bfabc0147935fffb5937f7b0bc7
jgsoto <sotoj9268@gmail.com>
2026-04-28 08:00:30 -0500

    initial commit

---
+++ src/main/java/org/uce/spaguettimonolitic/SpaguettiMonoliticApplication.java
@@ -0,0 +1,13 @@
+package org.uce.spaguettimonolitic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaguettiMonoliticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaguettiMonoliticApplication.class, args);
+	}
+
+}
